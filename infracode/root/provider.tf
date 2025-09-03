20e5a62311c8b1cd79a38c039d43c326a8e0ddfe
pramod <prmd777@rediffmail.com>
2025-09-03 10:00:41 +0530

    modified

--- infracode/root/provider.tf
+++ infracode/root/provider.tf
@@ -13,8 +13,11 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
-
-  subscription_id = "7080016c-eeaa-474a-9e8c-530ded9fb9f8"
+ subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  
 }
 
 data "azurerm_client_config" "current" {}
