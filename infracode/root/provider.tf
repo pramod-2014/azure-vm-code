4c0510014c7356ac84a36936ac3edb6aedc0ef34
pramod <prmd777@rediffmail.com>
2025-09-03 10:25:52 +0530

    added dependencies

--- infracode/root/provider.tf
+++ infracode/root/provider.tf
@@ -13,10 +13,12 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
- subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+ subscription_id = "a29ac6ee-0d4f-41fe-8359-df26a6fce56c"
+
+#  subscription_id = var.subscription_id
+#   client_id       = var.client_id
+#   client_secret   = var.client_secret
+#   tenant_id       = var.tenant_id
   
 }
 
